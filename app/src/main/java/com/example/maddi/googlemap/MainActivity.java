22db02471ddfdf9c18b4cd021c579d359b9c12d0
Ewaoluwa Ogunneye <zamorite@gmail.com>
2020-10-15 22:29:43 +0400

    Refactor code to make markers visible

--- app/src/main/java/com/example/maddi/googlemap/MainActivity.java
+++ app/src/main/java/com/example/maddi/googlemap/MainActivity.java
@@ -47,7 +47,8 @@ import okhttp3.Response;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
-    ArrayList<LatLng> arrayList = new ArrayList<LatLng>();
+    ArrayList<LatLng> coordList = new ArrayList<LatLng>();
+    ArrayList<String> locationLabels = new ArrayList<String>();
 
    /* LatLng sydney = new LatLng(-34,151);
     LatLng TamWorth = new LatLng(-31.083332,150.916672);
@@ -67,135 +68,102 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         /*arrayList.add(sydney);
         arrayList.add(TamWorth);
         arrayList.add(NewCastle);
         arrayList.add(Brisbane);
-        arrayList.add(Dubbo);
-*/
+        arrayList.add(Dubbo);*/
 
         searchView = findViewById(R.id.sv_location);
-
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
 
-       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-
-               String location = searchView.getQuery().toString();
-
-                List<Address> addressList = null;
-
-                if (location != null || !location.equals(""))
-                {
-                    Geocoder geocoder = new Geocoder(MainActivity.this);
-                    try {
-                             addressList = geocoder.getFromLocationName(location,1);
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    for (Address address : addressList) {
-                        LatLng  latLng = new LatLng(address.getLatitude(),address.getLongitude());
-                        map.addMarker(new MarkerOptions().position(latLng).title(location));
-                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-
-                       // map.addMarker(new MarkerOptions().position(latLng).title(location));
-                    }
-
-//                    Address address = addressList.get(0);
-                    int listSize = addressList.size();
-                    if (listSize > 0) {
-                        Address lastAddress = addressList.get(listSize - 1);
-
-                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastAddress.getLatitude(),lastAddress.getLongitude()),10));
-
-                    }
-                }
-                return false;
+                return fetchData(query);
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                // mTextViewResult = findViewById(R.id.text_view_result);
-
-                RequestBody formBody = new FormBody.Builder()
-                        .add("searchText", newText)
-                        .add("apiKey", "501edc9e")
-                        .build();
-
-                Request request = new Request.Builder()
-                        .url(url)
-                        .post(formBody)
-                        .build();
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        e.printStackTrace();
-                    }
-                    @Override
-                    public void onResponse(Call call, final Response response) throws IOException {
-                        if (response.isSuccessful()) {
-                            final String myResponse = response.body().string();
-                            MainActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    readJson(myResponse);
-//                            arrayList.add(myResponse);
-                                    //  mTextViewResult.setText(myResponse);
-                                }
-                            });
-                        }
-                    }
-                });
-
-                return false;
+                return fetchData(newText);
             }
         });
 
         mapFragment.getMapAsync(this);
 
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         map = googleMap;
 
-        for (int i = 0 ; i< arrayList.size(); i++)
+        for (int i = 0 ; i< coordList.size(); i++)
         {
-            map.addMarker(new MarkerOptions().position(arrayList.get(i)).title("Marker"));
+            map.addMarker(new MarkerOptions().position(coordList.get(i)).title("Marker"));
             map.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
-            map.moveCamera(CameraUpdateFactory.newLatLng(arrayList.get(i)));
-
+            map.moveCamera(CameraUpdateFactory.newLatLng(coordList.get(i)));
         }
+
     }
-    public void readJson (String json_string)
+
+    public void  readJson  (String json_string)
     {
-//        String json_string = null;
-
-//            InputStream inputStream = getAssets().open("myResponse");
-//            int size = inputStream.available();
-//            byte[] buffer = new byte[size];
-//            inputStream.read(buffer);
-//            inputStream.close();
-//
-//            json_string = new String(buffer,"UTF-8");
-
-//            Toast.makeText(getApplicationContext(),json_string, Toast.LENGTH_SHORT).show();
 
             Gson gson = new Gson();
             Post post = gson.fromJson(json_string,Post.class);
 
+            coordList = new ArrayList<LatLng>();
+            locationLabels = new ArrayList<String>();
+
             for (LocationData data : post.locationData){
-             arrayList.add(new LatLng(data.latitude,data.longitude));
+                coordList.add(new LatLng(data.latitude, data.longitude));
+                locationLabels.add(data.name);
             }
 
-
-            //Toast.makeText(getApplicationContext(), post.father_details"", Toast.LENGTH_SHORT).show();
-
-        }
-
     }
+
+    public void  showMarkers() {
+        for (int i = 0; i < coordList.size(); i++) {
+            map.addMarker(new MarkerOptions().position(coordList.get(i)).title(locationLabels.get(i)));
+        }
+    }
+
+    public boolean fetchData(String query) {
+        RequestBody formBody = new FormBody.Builder()
+                .add("searchText", query)
+                .add("apiKey", "501edc9e")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            readJson(myResponse);
+                            showMarkers();
+                        }
+                    });
+                }
+            }
+        });
+
+        return false;
+    }
+
+}
